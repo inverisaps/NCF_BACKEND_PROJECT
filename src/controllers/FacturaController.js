7d4e88de725cbcab8bb1d2e56ddc60f54c9a21aa
ElianMartinez <elianmartinez157@gmail.com>
2021-03-26 11:20:49 -0400

    completo, descomprecion se archivos. y instalacion de node-cron

--- src/controllers/FacturaController.js
+++ src/controllers/FacturaController.js
@@ -29,4 +29,6 @@ else{
 }
 };
 
+
 module.exports = { CreateFactura };
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const FileClass = require("./assets/test");
+var clas = new FileClass();
 //settings 
 app.set("port", 8999);
 //Middlewares 
@@ -11,4 +13,15 @@ app.use(require('./routes/route'));
 //Starting server
 app.listen(app.get('port'), () => {
     console.log("server on port "+app.get('port'));
+    
 });
+
+
+// async function goo(file, desc) {
+//     var start = Date.now();
+//     var clas = new FileClass();
+//     await clas.proceso(file, desc);
+//     var end = Date.now();
+//     console.log((end - start) / 1000 + " seg");
+//   }
+//   goo("./src/assets/file.zip","./src/assets/");
