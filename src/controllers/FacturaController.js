77b93a34d1d29ce36dab0118af7d189b95f2b145
ElianMartinez <elianmartinez157@gmail.com>
2021-05-07 11:26:41 -0400

    arreglos de networking

--- src/controllers/FacturaController.js
+++ src/controllers/FacturaController.js
@@ -7,7 +7,7 @@ const CreateFactura = async (req, res) => {
   api_key.verificar(req, res, async (reqe, rese) => {
     //create funcion
     const data = reqe.body;
-    
+
     if (Object.keys(data).length == 7) {
       if (
         data.id_t_n !== null &&
@@ -15,11 +15,13 @@ const CreateFactura = async (req, res) => {
         data.id_t_n !== ""
       ) {
         let resultado = await FModel.CreateFactura(data);
-        if (resultado) {
+        if (resultado !== "error min") {
           rese.json({
             res: "ok",
             data: resultado[0],
           });
+        }else if(resultado === "error min") {
+          rese.status(400);
         } else {
           rese.json({
             res: "false",
--- src/models/FacuturaModel.js
+++ src/models/FacuturaModel.js
@@ -1,21 +1,39 @@
 const db = require("../DB/db");
 module.exports = class FacturaModel {
+  async selectMin(id) {
+    const sql = `select (HASTA - DESDE) as valor from tipo_ncf where ID_NFC = ${id}`;
+    try {
+      let resul = await db.query(sql);
+      if (resul[0].valor > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   async CreateFactura(data) {
-    console.log(data);
-    const  fech = data.fecha.split(" ")[0];
-    const  hora = data.fecha;
+    const fech = data.fecha.split(" ")[0];
+    const hora = data.fecha;
     try {
       let sql = `Call add_factura(${data.id_t_n},'${data.rnc}','${data.nombre}',${data.monto}, ${data.id_sucursal}, ${data.id_metodo_pago}, '${fech}', '${hora}');`;
+     let auth = await this.selectMin(data.id_t_n);
+     if(auth){
       let resul = await db.query(sql);
- 
       if (resul[1].affectedRows > 0) {
         return resul[0];
       } else {
-        return false; 
+        return false;
       }
-    } catch (err) {
+    }else{
+      return "error min";
+    }
+    }catch(err) {
       console.log(err);
       return err;
     }
   }
+
 };
