82fb59b9dc149a995ab2338f1d7d6bf50c7c0fcc
ElianMartinez <elianmartinez157@gmail.com>
2021-05-24 11:13:29 -0400

    version 1.1.0 Forma con API sin archivo

--- src/controllers/RncController.js
+++ src/controllers/RncController.js
@@ -1,49 +1,59 @@
 const fs = require("fs");
+const axios = require("axios");
 const API_KEY = require("./API-KEY");
 var api_key = new API_KEY();
 function findLine(rnc, fn) {
-  let MAX_SENTENCE_LENGTH = 207;
-  var response = "";
-  fs.readFile("./src/assets/TMP/DGII_RNC.TXT", "utf-8", function (err, data) {
-    let startPoint = data.search(rnc);
-    if (startPoint === -1) {
-      fn(false);
-    } else {
-      let extract = data.substring(
-        startPoint,
-        startPoint + MAX_SENTENCE_LENGTH
+  const data = `
+  <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+  <GetContribuyentes xmlns="http://dgii.gov.do/">
+  <value>${rnc}</value>
+  <patronBusqueda>0</patronBusqueda>
+  <inicioFilas>0</inicioFilas>
+  <filaFilas>0</filaFilas>
+  <IMEI>?</IMEI>
+  </GetContribuyentes>
+  </soap:Body>
+  </soap:Envelope>
+  `;
+  var config = {
+    headers: {
+      "Content-Type": "text/xml; charset=utf-8",
+      "Content-Length": data.length,
+    },
+  };
+  axios
+    .post("http://dgii.gov.do/wsMovilDGII/WSMovilDGII.asmx", data, config)
+    .then((res) => {
+      const resp = JSON.parse(
+        res.data
+          .toString()
+          .split("<GetContribuyentesResult>")[1]
+          .split("</GetContribuyentesResult>")[0]
       );
-      let arrayOfSolution = extract.split("\n");
-      response = arrayOfSolution[0];
-      fn(response);
-    }
-  });
+      if (resp !== 0) {
+        fn(resp.RGE_NOMBRE);
+      } else {
+        fn(false);
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 const BuscarRNC = async (req, res) => {
   api_key.verificar(req, res, async (reqe, rese) => {
     const rnc = reqe.params.rnc;
     if (rnc !== null && rnc !== undefined && rnc !== "") {
-      await findLine(rnc.toString(), function (resp) {
-        let datita = "";
-        if (resp === false) {
-          datita = "none";
+      findLine(rnc.toString(), function (resp) {
+        if (resp) {
           rese.json({
             res: "ok",
-            data: datita.trim(),
+            data: resp,
           });
         } else {
-          resp
-            .split("|")[1]
-            .split(" ")
-            .forEach((element) => {
-              if (element !== "") {
-                datita += element + " ";
-              }
-            });
           rese.json({
             res: "ok",
-            data: datita.trim(),
+            data: "none",
           });
         }
       });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const FileClass = require("./assets/test");
-const cron = require("node-cron");
-const clas = new FileClass();
 //settings
 app.set("port", 8999);
 //Middlewares
@@ -11,15 +8,7 @@ app.use(express.json());
 app.use(cors());
 //Routes
 app.use(require("./routes/route"));
-
 //Starting server
-async function goo(file, desc) {
-  await clas.proceso(file, desc);
-}
-
 app.listen(app.get("port"), () => {
   console.log("server on port " + app.get("port"));
-  cron.schedule("0 10 * * *", () => {
-    goo("./src/assets/file.zip", "./src/assets/");
-  });
 });
